0ed747869a9fa4f473de308b5c6309557011a2ee
scrumpygeo <scrumpygeo@yahoo.ca>
2020-03-09 14:39:38 +0000

    create SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import SearchBar from './components/SearchBar';
 import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='left-scene'>
+          <SearchBar />
+        </div>
+        <div className='right-scene'></div>
+      </div>
+    );
+  }
+}
 
 export default App;
